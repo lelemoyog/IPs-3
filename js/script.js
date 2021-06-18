188bd8c55bcd9b44fa51c7105802d8b7db31947e
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-06-18 17:13:46 +0300

    added footer

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    
+    
+    $("img#w1").click(function () {
+        alert("design")
+    })
+    $("img#w2").click(function () {
+        alert("design")
+    })
+    $("img#w3").click(function () {
+        alert("design")
+    })
+
+});
