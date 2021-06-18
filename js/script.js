b19493c1348ee7c489032bf476d69b37e661c5b2
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-06-18 18:31:54 +0300

    added toggle to 4th container

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
+    $("div#ww1").hide();
+    $("div#ww2").hide();
+    $("div#ww3").hide();
     
+    $("img#w1 , div#ww1").click(function () {
+        $("div#ww1, img#w1").toggle();
     
-    $("img#w1").click(function () {
-        alert("design")
     })
-    $("img#w2").click(function () {
-        alert("design")
+    $("img#w2 ,div#ww2").click(function () {
+        $("div#ww2 , img#w2").toggle();
     })
-    $("img#w3").click(function () {
-        alert("design")
+    $("img#w3 ,div#ww3").click(function () {
+        $("div#ww3 , img#w3").toggle();
     })
 
 });
